774c8ffad9a70ec9663e56929caa4b7fa6a4998e
sedtmp <smegdm@yandex.ru>
2021-12-10 18:19:27 +0300

    Scripted auto-commit on change (2021-12-10 18:19:27) by gitwatch.sh

--- lab2/src/main/java/AirportGroupingComparator.java
+++ lab2/src/main/java/AirportGroupingComparator.java
@@ -2,6 +2,6 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class AirportGroupingComparator extends WritableComparator {
     public AirportGroupingComparator() {
-        super(AirportGroupingComparator)
+        super(AirportGroupingComparator.class, true);
     }
 }
