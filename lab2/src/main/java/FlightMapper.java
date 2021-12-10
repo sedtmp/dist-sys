279c570512a2aa10790c74a1f943ffed483ac948
sedtmp <smegdm@yandex.ru>
2021-12-10 18:53:12 +0300

    Scripted auto-commit on change (2021-12-10 18:53:12) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -2,10 +2,14 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     private static final int AIRPORT_ID = 14;
     private static final int DELAY = 18;
 
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IO
+    protected void map(LongWritable key, Text value, Context context) throws IOException {
+        
+    }
 }
