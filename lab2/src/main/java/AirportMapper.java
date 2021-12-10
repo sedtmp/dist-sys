530b62cb550801dcab6141fb0fd67d3468808433
sedtmp <smegdm@yandex.ru>
2021-12-10 17:29:27 +0300

    Scripted auto-commit on change (2021-12-10 17:29:27) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -2,12 +2,14 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     @Override
     protected void map(LongWritable key,
                        Text value,
                        Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context)
-            throws 
+            throws IOException
     {
 
     }
