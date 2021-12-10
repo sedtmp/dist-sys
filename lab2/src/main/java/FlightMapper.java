6efee9a615f15cacf37238234226f85cc264b2fa
sedtmp <smegdm@yandex.ru>
2021-12-10 18:52:48 +0300

    Scripted auto-commit on change (2021-12-10 18:52:48) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -7,5 +7,5 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
     private static final int DELAY = 18;
 
     @Override
-    protected void map(LongWritable)
+    protected void map(LongWritable key, )
 }
