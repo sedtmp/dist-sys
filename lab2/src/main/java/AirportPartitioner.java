471855dc71dcab492be5dfda236b7c4ac0c04cb3
sedtmp <smegdm@yandex.ru>
2021-12-10 18:08:26 +0300

    Scripted auto-commit on change (2021-12-10 18:08:26) by gitwatch.sh

--- lab2/src/main/java/AirportPartitioner.java
+++ lab2/src/main/java/AirportPartitioner.java
@@ -1,6 +1,6 @@
 import org.apache.hadoop.mapreduce.Partitioner;
-
-import org.apache.hadoop.Text;
+import org.apache.hadoop.io.Text;
 
 public class AirportPartitioner extends Partitioner<AirportWritableComparable, Text> {
+    
 }
