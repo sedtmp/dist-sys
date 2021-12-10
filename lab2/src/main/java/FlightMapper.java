a53e6ea9c62ec1f09e806f7d955442c1cf5944a0
sedtmp <smegdm@yandex.ru>
2021-12-10 19:06:41 +0300

    Scripted auto-commit on change (2021-12-10 19:06:41) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -5,16 +5,16 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
-    private static final int AIRPORT_ID = 14;
-    private static final int DELAY_INDEX = 18;
+    private static final int DEST_AIRPORT_ID = 14;
+    private static final int ARR_DELAY_NEW = 18;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException {
         String[] line = value.toString().split(",");
-        String airportIdStr = line[AIRPORT_ID];
+        String airportIdStr = line[DEST_AIRPORT_ID];
         if (!airportIdStr.equals("\"DEST_AIRPORT_ID\"")) {
             int airportId = Integer.parseInt(airportIdStr);
-            String delay = line[DELAY_INDEX];
+            String delay = line[ARR_DELAY_NEW];
             if (!delay.isEmpty()) {
                 context.write(new AirportWritableComparable(airportId, 1), new Text(delay));
             }
