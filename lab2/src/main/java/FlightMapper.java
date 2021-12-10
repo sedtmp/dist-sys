6ccfbf01f5cdd5496087cc855f967f17c3771b55
sedtmp <smegdm@yandex.ru>
2021-12-10 18:50:16 +0300

    Scripted auto-commit on change (2021-12-10 18:50:16) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -3,5 +3,5 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
-    private static int AIRPORT_ID = 
+    private static int AIRPORT_ID = 1
 }
