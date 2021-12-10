8ebd9ccae9e04866184ca30da841312d714ff595
sedtmp <smegdm@yandex.ru>
2021-12-11 02:49:29 +0300

    Scripted auto-commit on change (2021-12-11 02:49:29) by gitwatch.sh

--- lab2/src/main/java/DelayReducer.java
+++ lab2/src/main/java/DelayReducer.java
@@ -10,6 +10,6 @@ public class DelayReducer extends Reducer<AirportWritableComparable, Text, Text,
                           Iterable<Text> values,
                           Context context)
             throws IOException, InterruptedException {
-        Iterator<Text> 
+        Iterator<Text> iter = 
     }
 }
