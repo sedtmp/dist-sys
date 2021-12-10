f236c80ab4a7affb90e6cb79710418d97df0b2af
sedtmp <smegdm@yandex.ru>
2021-12-10 17:05:57 +0300

    Scripted auto-commit on change (2021-12-10 17:05:57) by gitwatch.sh

--- lab2/src/main/java/AirportWritableComparable.java
+++ lab2/src/main/java/AirportWritableComparable.java
@@ -2,5 +2,5 @@ import org.apache.hadoop.io.WritableComparable;
 
 public class AirportWritableComparable implements WritableComparable<AirportWritableComparable> {
     int airportId;
-    
+    int 
 }
