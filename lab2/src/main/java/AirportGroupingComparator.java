10b42b4e198282d1d0347bf9852ea7c2d4994760
sedtmp <smegdm@yandex.ru>
2021-12-10 18:20:04 +0300

    Scripted auto-commit on change (2021-12-10 18:20:04) by gitwatch.sh

--- lab2/src/main/java/AirportGroupingComparator.java
+++ lab2/src/main/java/AirportGroupingComparator.java
@@ -6,5 +6,5 @@ public class AirportGroupingComparator extends WritableComparator {
     }
 
     @Override
-    public int compare(WritableComparator)
+    public int compare(WritableComparator first, )
 }
