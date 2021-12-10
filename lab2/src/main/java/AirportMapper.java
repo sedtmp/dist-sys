60b06400947a836e66e5f88b667bd63a89fc3bb1
sedtmp <smegdm@yandex.ru>
2021-12-10 17:27:25 +0300

    Scripted auto-commit on change (2021-12-10 17:27:25) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -1,5 +1,6 @@
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class AirportMapper extends Mapper<LongWritable> {
+public class AirportMapper extends Mapper<LongWritable, Text, > {
 }
