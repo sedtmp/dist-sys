25203cdeb1b30a4ab47d6f5d680cb88b41f17a9e
sedtmp <smegdm@yandex.ru>
2021-12-10 19:37:49 +0300

    Scripted auto-commit on change (2021-12-10 19:37:49) by gitwatch.sh

--- lab2/src/main/java/DelayReducer.java
+++ lab2/src/main/java/DelayReducer.java
@@ -1,5 +1,6 @@
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class DelayReducer extends Reducer<AirportWritableComparable, > {
+public class DelayReducer extends Reducer<AirportWritableComparable, Text, Text, Text> {
 
 }
