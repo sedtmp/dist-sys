c00bc1a068bc4f0df9753e37346fc6dd291bf8a5
sedtmp <smegdm@yandex.ru>
2021-12-10 17:32:26 +0300

    Scripted auto-commit on change (2021-12-10 17:32:26) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -13,7 +13,7 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableCom
         String[] line = value.toString().split(",[\"]");
         if (key.get() > 0) {
             String code = line[0].replaceAll("\"", "");
-            String description = 
+            String description = line[1].
         }
     }
 }
