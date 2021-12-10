abb274ee0102701b245bc20bd4a298cab30e7a82
sedtmp <smegdm@yandex.ru>
2021-12-10 17:30:26 +0300

    Scripted auto-commit on change (2021-12-10 17:30:26) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -10,6 +10,6 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableCom
                        Text value,
                        Mapper<LongWritable, Text, AirportWritableComparable, Text>.Context context)
             throws IOException, InterruptedException {
-        String[] line = 
+        String[] line = value.toString()
     }
 }
