2aef89d6d4989bb38bdb6ebd20b71a6bbc90c1a8
sedtmp <smegdm@yandex.ru>
2021-12-10 18:28:55 +0300

    Scripted auto-commit on change (2021-12-10 18:28:55) by gitwatch.sh

--- lab2/src/main/java/AirportGroupingComparator.java
+++ lab2/src/main/java/AirportGroupingComparator.java
@@ -9,6 +9,6 @@ public class AirportGroupingComparator extends WritableComparator {
     public int compare(WritableComparator first, WritableComparator second) {
         AirportWritableComparable firstAwc = (AirportWritableComparable) first;
         AirportWritableComparable secondAwc = (AirportWritableComparable) second;
-        return Integer.compare()
+        return Integer.compare(firstAwc.getAirportId(), secondAwc.getAirportId());
     }
 }
