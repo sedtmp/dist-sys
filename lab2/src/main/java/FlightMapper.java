1e56a40c8c833d8ba4a53c99271b397602148d5f
sedtmp <smegdm@yandex.ru>
2021-12-10 18:58:21 +0300

    Scripted auto-commit on change (2021-12-10 18:58:21) by gitwatch.sh

--- lab2/src/main/java/FlightMapper.java
+++ lab2/src/main/java/FlightMapper.java
@@ -15,7 +15,9 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
         if (!airportIdStr.equals("\"DEST_AIRPORT_ID\"")) {
             int airportId = Integer.parseInt(airportIdStr);
             String delay = line[DELAY];
-            if (!delay.isEmpty())
+            if (!delay.isEmpty()) {
+                context.write();
+            }
         }
     }
 }
