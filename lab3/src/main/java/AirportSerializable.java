661fb1981539ade739bd0d93e2f121b5c21deb69
sedtmp <smegdm@yandex.ru>
2021-12-11 11:11:56 +0300

    Scripted auto-commit on change (2021-12-11 11:11:56) by gitwatch.sh

--- lab3/src/main/java/AirportSerializable.java
+++ lab3/src/main/java/AirportSerializable.java
@@ -1,26 +1,14 @@
 import java.io.Serializable;
 
 public class AirportSerializable implements Serializable {
-    private int originAirportId;
-    private int destAirportId;
     private float delay;
     private boolean cancelled;
 
     public AirportSerializable() {}
 
-    public AirportSerializable(int originAirportId, int destAirportId, float delay, boolean cancelled) {
-        this.originAirportId = originAirportId;
-        this.destAirportId = destAirportId;
+    public AirportSerializable(float delay, boolean cancelled) {
         this.delay = delay;
         this.cancelled = cancelled;
-    }
-
-    public int getOriginAirportId() {
-        return originAirportId;
-    }
-
-    public int getDestAirportId() {
-        return destAirportId;
     }
 
     public float getDelay() {
@@ -29,14 +17,6 @@ public class AirportSerializable implements Serializable {
 
     public boolean isCancelled() {
         return cancelled;
-    }
-
-    public void setOriginAirportId(int originAirportId) {
-        this.originAirportId = originAirportId;
-    }
-
-    public void setDestAirportId(int destAirportId) {
-        this.destAirportId = destAirportId;
     }
 
     public void setDelay(float delay) {
