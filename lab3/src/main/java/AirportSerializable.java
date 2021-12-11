81c80a9175fe4a4ee189f74112673c40883a5ebf
sedtmp <smegdm@yandex.ru>
2021-12-11 09:02:27 +0300

    Scripted auto-commit on change (2021-12-11 09:02:27) by gitwatch.sh

--- lab3/src/main/java/AirportSerializable.java
+++ lab3/src/main/java/AirportSerializable.java
@@ -3,5 +3,6 @@ import java.io.Serializable;
 public class AirportSerializable implements Serializable {
     private int originAirportId;
     private int destAirportId;
-    private 
+    private float delay;
+    
 }
