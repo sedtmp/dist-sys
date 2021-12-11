abe6750cc7cf4023b38f67bb53dc0f9420184de6
sedtmp <smegdm@yandex.ru>
2021-12-11 10:49:18 +0300

    Scripted auto-commit on change (2021-12-11 10:49:18) by gitwatch.sh

--- lab3/src/main/java/App.java
+++ lab3/src/main/java/App.java
@@ -3,10 +3,11 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 public class App {
     private final static String airportsPath = "resources/L_AIRPORT_ID.csv";
-    private final static String fligthsPath = "resources/L_AIRPORT_ID.csv";
+    private final static String fligthsPath = "resources/664600583_T_ONTIME_sample.csv";
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
+        
     }
 }
