bf996cc93689807e25bc2484fbe90468a3f46249
sedtmp <smegdm@yandex.ru>
2021-12-11 03:15:53 +0300

    Scripted auto-commit on change (2021-12-11 03:15:53) by gitwatch.sh

--- lab2/src/main/java/App.java
+++ lab2/src/main/java/App.java
@@ -17,6 +17,7 @@ public class App {
 
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
+        FileOu
 
         job.setPartitionerClass(AirportPartitioner.class);
         job.setGroupingComparatorClass(AirportGroupingComparator.class);
