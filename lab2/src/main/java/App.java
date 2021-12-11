d492d23e2bd23ac322c2a09905b5ef05a7740abd
sedtmp <smegdm@yandex.ru>
2021-12-11 03:31:57 +0300

    Scripted auto-commit on change (2021-12-11 03:31:57) by gitwatch.sh

--- lab2/src/main/java/App.java
+++ lab2/src/main/java/App.java
@@ -2,7 +2,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.lib.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
 
 public class App {
