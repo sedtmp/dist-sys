7e5980290877eb89c72578fc25ef72248183d096
sedtmp <smegdm@yandex.ru>
2021-12-11 03:20:53 +0300

    Scripted auto-commit on change (2021-12-11 03:20:53) by gitwatch.sh

--- lab2/src/main/java/App.java
+++ lab2/src/main/java/App.java
@@ -1,6 +1,7 @@
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.TextInputFormat;
 import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
@@ -12,7 +13,7 @@ public class App {
             System.exit(-1);
         }
 
-        Job job = Job.getInstance();
+        JobConf job = Job.getInstance();
         job.setJarByClass(App.class);
         job.setJobName("Make join");
 
