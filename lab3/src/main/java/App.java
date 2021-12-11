82693ea02e08e7555c118bc702b757ead64f0c8a
sedtmp <smegdm@yandex.ru>
2021-12-11 12:34:49 +0300

    Scripted auto-commit on change (2021-12-11 12:34:49) by gitwatch.sh

--- lab3/src/main/java/App.java
+++ lab3/src/main/java/App.java
@@ -65,7 +65,7 @@ public class App {
                 T -> {
                     Map<Integer, String> airportsData = airportsBroadcasted.value();
                     String outputStr = airportsData.get(T._1._1) + " -> " + airportsData.get(T._1._2) + " :\n";
-                    outputStr += "maxDelay: " + 
+                    outputStr += "maxDelay: " + T._2.
                 }
         );
     }
