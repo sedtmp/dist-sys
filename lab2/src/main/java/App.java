3b45187f38a8c276fabcfaa8065ef36e9b994ac4
sedtmp <smegdm@yandex.ru>
2021-12-11 03:03:50 +0300

    Scripted auto-commit on change (2021-12-11 03:03:50) by gitwatch.sh

--- lab2/src/main/java/App.java
+++ lab2/src/main/java/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
-            System.err.println();
+            System.err.println("Usage");
         }
     }
 }
