bf02963a4413cc6f1e5af0e17bb6c410a7e6ae52
sedtmp <smegdm@yandex.ru>
2021-12-11 10:46:10 +0300

    Scripted auto-commit on change (2021-12-11 10:46:10) by gitwatch.sh

--- lab3/src/main/java/App.java
+++ lab3/src/main/java/App.java
@@ -4,6 +4,8 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class App {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
-        JavaSparkContext sc = new JavaSparkContext()
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        
     }
 }
