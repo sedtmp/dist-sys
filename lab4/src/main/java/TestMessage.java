510d9bfecbb10af32078d7f37e6173fc29432cff
sedtmp <smegdm@yandex.ru>
2021-12-17 16:39:13 +0300

    Scripted auto-commit on change (2021-12-17 16:39:13) by gitwatch.sh

--- lab4/src/main/java/TestMessage.java
+++ lab4/src/main/java/TestMessage.java
@@ -10,6 +10,7 @@ public class TestMessage {
     @JsonCreator
     public TestMessage(
             @JsonProperty(Constants.PACKAGE_ID) Integer packageId,
-            @JsonProperty(Constants.JS_SCRIPT) String 
+            @JsonProperty(Constants.JS_SCRIPT) String jsScript,
+            
     )
 }
