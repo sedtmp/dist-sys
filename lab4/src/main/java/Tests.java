b19c151e2e801f5244cc487e43588f57f0723340
sedtmp <smegdm@yandex.ru>
2021-12-17 13:28:37 +0300

    Scripted auto-commit on change (2021-12-17 13:28:37) by gitwatch.sh

--- lab4/src/main/java/Tests.java
+++ lab4/src/main/java/Tests.java
@@ -15,6 +15,7 @@ public class Tests {
 
     @JsonCreator
     public Tests(
-            @JsonProperty(PACKAGE_ID) Integer
+            @JsonProperty(PACKAGE_ID) Integer packageId,
+            
     )
 }
