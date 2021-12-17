8d64205cdd29677eaeb33d899cbed725e7236e7f
sedtmp <smegdm@yandex.ru>
2021-12-17 23:31:23 +0300

    Scripted auto-commit on change (2021-12-17 23:31:23) by gitwatch.sh

--- lab4/src/main/java/App.java
+++ lab4/src/main/java/App.java
@@ -14,6 +14,7 @@ public class App {
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
         ActorRef storeActor = system.actorOf(Props.create(StoreActor.class), Constants.STORE_ACTOR);
+        
         ActorRef executeActor = system.actorOf(Props.create(ExecuteActor.class), )
     }
 }
