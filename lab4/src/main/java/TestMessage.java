6d3addc621aa0a0e219e47d38e1c49ff42400bb9
sedtmp <smegdm@yandex.ru>
2021-12-17 16:38:31 +0300

    Scripted auto-commit on change (2021-12-17 16:38:31) by gitwatch.sh

--- lab4/src/main/java/TestMessage.java
+++ lab4/src/main/java/TestMessage.java
@@ -1,8 +1,10 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public class TestMessage {
     private final Integer packageId;
     private final String jsScript;
     private final String functionTitle;
     private final Test test;
 
-    
+    @JsonCreator
 }
