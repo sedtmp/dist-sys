160db98da4912e8186567a650f9a9ac79d7e27bb
sedtmp <smegdm@yandex.ru>
2021-12-17 13:26:18 +0300

    Scripted auto-commit on change (2021-12-17 13:26:18) by gitwatch.sh

--- lab4/src/main/java/Tests.java
+++ lab4/src/main/java/Tests.java
@@ -3,7 +3,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Tests {
     private final String PACKAGE_ID = "packageId";
     private final String PACKAGE_ID = "packageId";
-    private final String PACKAGE_ID = "packageId";
 
     @JsonProperty
 }
