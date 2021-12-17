47e535b62ffd8289c657e35ca7bc2086957b2c2e
sedtmp <smegdm@yandex.ru>
2021-12-17 13:23:04 +0300

    Scripted auto-commit on change (2021-12-17 13:23:04) by gitwatch.sh

--- lab4/src/main/java/TestsStorage.java
+++ lab4/src/main/java/TestsStorage.java
@@ -10,6 +10,6 @@ public class TestsStorage extends AbstractActor {
 
     @Override
     public Receive createRecieve() {
-        return ReceiveBuilder.create().
+        return ReceiveBuilder.create().match()
     }
 }
