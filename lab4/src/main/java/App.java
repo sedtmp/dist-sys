63c332507fa95cd71cd54509026a1fb373bb4c35
sedtmp <smegdm@yandex.ru>
2021-12-17 23:32:30 +0300

    Scripted auto-commit on change (2021-12-17 23:32:30) by gitwatch.sh

--- lab4/src/main/java/App.java
+++ lab4/src/main/java/App.java
@@ -4,16 +4,18 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.routing.RoundRobinPool;
 import akka.stream.ActorMaterializer;
 import constants.Constants;
 
 public class App {
+    
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("lab4");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
         ActorRef storeActor = system.actorOf(Props.create(StoreActor.class), Constants.STORE_ACTOR);
-        ActorRef executeActor = system.actorOf(Props.create(ExecuteActor.class), )
+        ActorRef executeActor = system.actorOf(new RoundRobinPool(), )
     }
 }
