aca75d5500a3aa89406fb8a418811b264fb6f2bd
sedtmp <smegdm@yandex.ru>
2021-12-17 19:04:20 +0300

    Scripted auto-commit on change (2021-12-17 19:04:20) by gitwatch.sh

--- lab4/src/main/java/StoreActor.java
+++ lab4/src/main/java/StoreActor.java
@@ -10,7 +10,8 @@ public class StoreActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder.create().match(
+        return ReceiveBuilder.create()
+                .match(
                 StoreMessage.class, m -> {
                     if (storage.containsKey(m.getPackageId())) {
                         ArrayList<Test> results = storage.get(m.getPackageId());
@@ -19,8 +20,7 @@ public class StoreActor extends AbstractActor {
                         return;
                     }
                     storage.put(m.getPackageId(), m.getTests());
-                }
-        ).match(GetMessage.class, req -> sender().tell(
+                }).match(GetMessage.class, req -> sender().tell(
                 new StoreMessage(req.getPackageId(), storage.get(req.getPackageId())), self())
         ).build();
     }
