65ebc7443cabb47e258c2cc2bbd3a18368c1c637
sedtmp <smegdm@yandex.ru>
2021-12-17 20:38:30 +0300

    Scripted auto-commit on change (2021-12-17 20:38:30) by gitwatch.sh

--- lab4/src/main/java/actors/StoreActor.java
+++ lab4/src/main/java/actors/StoreActor.java
@@ -2,20 +2,22 @@ package actors;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import messages.StoreMessage;
+import tests.TestResult;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StoreActor extends AbstractActor {
-    final Map<Integer, ArrayList<Test>> storage = new HashMap<>();
+    final Map<Integer, ArrayList<TestResult>> storage = new HashMap<>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(StoreMessage.class, m -> {
                     if (storage.containsKey(m.getPackageId())) {
-                        ArrayList<Test> results = storage.get(m.getPackageId());
+                        ArrayList<TestResult> results = storage.get(m.getPackageId());
                         results.addAll(m.getTests());
                         storage.replace(m.getPackageId(), results);
                         return;
