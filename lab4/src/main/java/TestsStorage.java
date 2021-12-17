0359d128c7cffbdfea71d2f219b5a0c4788687e4
sedtmp <smegdm@yandex.ru>
2021-12-17 13:19:35 +0300

    Scripted auto-commit on change (2021-12-17 13:19:35) by gitwatch.sh

--- lab4/src/main/java/TestsStorage.java
+++ lab4/src/main/java/TestsStorage.java
@@ -1,5 +1,8 @@
 import akka.actor.AbstractActor;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class TestsStorage extends AbstractActor {
-    final Map<Integer, ArrayList<>>
+    final Map<Integer, ArrayList<Test>> 
 }
