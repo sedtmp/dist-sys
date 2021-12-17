b8ae91e80570d7a2a29f7fe436e987cdb6fa34da
sedtmp <smegdm@yandex.ru>
2021-12-17 16:13:24 +0300

    Scripted auto-commit on change (2021-12-17 16:13:24) by gitwatch.sh

--- lab4/src/main/java/StoreActor.java
+++ lab4/src/main/java/StoreActor.java
@@ -20,8 +20,6 @@ public class StoreActor extends AbstractActor {
                     }
                     storage.put(msg.getPackageId(), msg.getTests());
                 }
-        ).match(GetMessage.class, req -> sender().tell(
-                new StoreMessage(req.getPackageId(), storage.get(req.getPackageId())), self())
-        ))
+        ).match(GetMessage.class, req -> sender().tell(new StoreMessage(req.getPackageId(), storage.get(req.getPackageId())), self())))
     }
 }
