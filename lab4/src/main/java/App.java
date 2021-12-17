57b04787c8e037eef012be7a8e1f1a312b17dcfd
sedtmp <smegdm@yandex.ru>
2021-12-18 00:34:04 +0300

    Scripted auto-commit on change (2021-12-18 00:34:04) by gitwatch.sh

--- lab4/src/main/java/App.java
+++ lab4/src/main/java/App.java
@@ -58,6 +58,6 @@ public class App {
                 storeActor,
                 routeActor
         ).flow(system, materializer);
-        final ConnectHttp connectHttp = ConnectHttp.toHost()
+        final ConnectHttp connectHttp = ConnectHttp.toHost(Constants.HOST, Constants)
     }
 }
