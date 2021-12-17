9b616d78994977b1527975a4637e92a6001aa077
sedtmp <smegdm@yandex.ru>
2021-12-17 16:33:01 +0300

    Scripted auto-commit on change (2021-12-17 16:33:01) by gitwatch.sh

--- lab4/src/main/java/TestMessage.java
+++ lab4/src/main/java/TestMessage.java
@@ -1,9 +1,11 @@
 public class TestMessage {
+    
+
     private final Integer packageId;
     private final String jsScript;
     private final String functionTitle;
     private final Test test;
 
-    
+
 
 }
