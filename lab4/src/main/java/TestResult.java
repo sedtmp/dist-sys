2720e929461c9c9da5d0acfbbea62c9ad9778d1d
sedtmp <smegdm@yandex.ru>
2021-12-17 20:25:35 +0300

    Scripted auto-commit on change (2021-12-17 20:25:35) by gitwatch.sh

--- lab4/src/main/java/TestResult.java
+++ lab4/src/main/java/TestResult.java
@@ -2,5 +2,9 @@ public class TestResult {
     private final Test test;
     private final String result;
 
-    public TestResult()
+    public TestResult(Test test, String result) {
+        this.test = test;
+        this.result = result;
+        
+    }
 }
