1f2a2b3086ea3bec1a4c1ee60cce5ce3c556a3b8
sedtmp <smegdm@yandex.ru>
2021-12-17 23:44:00 +0300

    Scripted auto-commit on change (2021-12-17 23:44:00) by gitwatch.sh

--- lab4/src/main/java/App.java
+++ lab4/src/main/java/App.java
@@ -17,7 +17,7 @@ public class App {
         ActorRef storeActor = system.actorOf(Props.create(StoreActor.class), Constants.STORE_ACTOR);
         ActorRef executeActor = system.actorOf(
                 new RoundRobinPool(Constants.POOL_NUMBER).props(Props.create(ExecuteActor.class)),
-                
-                )
+                Constants.EXECUTE_ACTOR
+        );
     }
 }
