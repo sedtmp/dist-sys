dee145d527d0c7744164f3323df8db32f10cc9b5
sedtmp <smegdm@yandex.ru>
2021-12-17 23:03:31 +0300

    Scripted auto-commit on change (2021-12-17 23:03:31) by gitwatch.sh

--- lab4/src/main/java/App.java
+++ lab4/src/main/java/App.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class App {
+import actors.StoreActor;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
+import constants.Constants;
+
+public class App {
+    public static void main(String[] args) {
+        ActorSystem system = ActorSystem.create("lab4");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        storeActor = system.actorOf(Props.create(StoreActor.class), Constants.STORE_ACTOR_PATH);
+    }
 }
