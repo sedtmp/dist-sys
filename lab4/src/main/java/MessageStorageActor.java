8139db73078018ba56004cc26e96f48c2e16e51c
sedtmp <smegdm@yandex.ru>
2021-12-17 15:55:23 +0300

    Scripted auto-commit on change (2021-12-17 15:55:23) by gitwatch.sh

--- lab4/src/main/java/MessageStorageActor.java
+++ lab4/src/main/java/MessageStorageActor.java
@@ -10,7 +10,7 @@ public class MessageStorageActor extends AbstractActor {
 
     public static class StoreMessage {
         private int packageId;
-        
+        private 
     }
 
     @Override
