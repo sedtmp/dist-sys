f32567d7043d2d5f3d6e1d9d1fb6fc92399a36ed
sedtmp <smegdm@yandex.ru>
2021-12-17 16:03:49 +0300

    Scripted auto-commit on change (2021-12-17 16:03:49) by gitwatch.sh

--- lab4/src/main/java/MessageStorageActor.java
+++ lab4/src/main/java/MessageStorageActor.java
@@ -9,36 +9,6 @@ import java.util.Map;
 public class MessageStorageActor extends AbstractActor {
     final Map<Integer, ArrayList<Test>> storage = new HashMap<>();
 
-    public static class StoreMessage {
-        private Integer packageId;
-        private String result;
-
-        public StoreMessage(Integer packageId, String result) {
-            this.packageId = packageId;
-            this.result = result;
-        }
-
-        public Integer getPackageId() {
-            return packageId;
-        }
-
-        public String getResult() {
-            return result;
-        }
-    }
-
-    public static class GetMessage {
-        private Integer packageId;
-
-        public GetMessage(Integer packageId) {
-            this.packageId = packageId;
-        }
-
-        public Integer getPackageId() {
-            return packageId;
-        }
-    }
-
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create().match(
