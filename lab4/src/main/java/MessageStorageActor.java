7da35630257828b1743213ece64fc37a467f0d17
sedtmp <smegdm@yandex.ru>
2021-12-17 15:55:02 +0300

    Scripted auto-commit on change (2021-12-17 15:55:02) by gitwatch.sh

--- lab4/src/main/java/MessageStorageActor.java
+++ lab4/src/main/java/MessageStorageActor.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class MessageStorageActor extends AbstractActor {
     final Map<Integer, ArrayList<Test>> storage = new HashMap<>();
 
-    public static 
+    public static class
 
     @Override
     public Receive createReceive() {
