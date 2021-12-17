6bda882b9ed7d73767ab799d9b361f65332078c0
sedtmp <smegdm@yandex.ru>
2021-12-17 16:37:15 +0300

    Scripted auto-commit on change (2021-12-17 16:37:15) by gitwatch.sh

--- lab4/src/main/java/Constants.java
+++ lab4/src/main/java/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
     public static final String PARAMS = "params";
     public static final String RESULT = "result";
     public static final String JS_SCRIPT = "jsScript";
-    public static final String FUNCTION_NAME = "functionName";  
+    public static final String FUNCTION_NAME = "functionName";
 }
