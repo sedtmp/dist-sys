b2125a01210c0c1a4b3349a33fd7858b8e460947
sedtmp <smegdm@yandex.ru>
2021-12-18 03:30:04 +0300

    Scripted auto-commit on change (2021-12-18 03:30:04) by gitwatch.sh

--- lab5/src/main/java/messages/StoreMessage.java
+++ lab5/src/main/java/messages/StoreMessage.java
@@ -4,5 +4,7 @@ public class StoreMessage {
     private String url;
     private Integer time;
 
-    public StoreMessage(String url)
+    public StoreMessage(String url, Integer time) {
+        
+    }
 }
