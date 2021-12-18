839408357d2a454c2fe1faeff3c8614f44e58fae
sedtmp <smegdm@yandex.ru>
2021-12-18 04:52:29 +0300

    Scripted auto-commit on change (2021-12-18 04:52:29) by gitwatch.sh

--- lab5/src/main/java/App.java
+++ lab5/src/main/java/App.java
@@ -15,6 +15,7 @@ public class App {
         ActorRef cacheActor = system.actorOf(Props.create(CacheActor.class));
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = 
+        FlowCreator creator = new FlowCreator()
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
     }
 }
