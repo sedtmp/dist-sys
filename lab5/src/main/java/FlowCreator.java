35e3dcd3ba6ba15daa1cf3a0dca8913e08eb09a0
sedtmp <smegdm@yandex.ru>
2021-12-18 03:51:34 +0300

    Scripted auto-commit on change (2021-12-18 03:51:34) by gitwatch.sh

--- lab5/src/main/java/FlowCreator.java
+++ lab5/src/main/java/FlowCreator.java
@@ -2,6 +2,7 @@ import akka.actor.ActorRef;
 import akka.stream.Materializer;
 
 public class FlowCreator {
+    
     private final ActorRef cacheActor;
     private final Materializer materializer;
 
