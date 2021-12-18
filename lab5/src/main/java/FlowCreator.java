8634d471a963943cedd91b264afeb426b7bcc755
sedtmp <smegdm@yandex.ru>
2021-12-18 04:06:03 +0300

    Scripted auto-commit on change (2021-12-18 04:06:03) by gitwatch.sh

--- lab5/src/main/java/FlowCreator.java
+++ lab5/src/main/java/FlowCreator.java
@@ -27,6 +27,6 @@ public class FlowCreator {
 
     public Flow<HttpRequest, HttpResponse, NotUsed> createFlow() {
         return Flow.of(HttpRequest.class)
-                .map
+                .map(req -> {})
     }
 }
