dc1ef302d135fcd8acb94e51c73651642b21ce5c
sedtmp <smegdm@yandex.ru>
2021-12-18 04:46:24 +0300

    Scripted auto-commit on change (2021-12-18 04:46:24) by gitwatch.sh

--- lab5/src/main/java/FlowCreator.java
+++ lab5/src/main/java/FlowCreator.java
@@ -80,7 +80,7 @@ public class FlowCreator {
                 })
                 .map(req -> {
                     cacheActor.tell(new StoreMessage(req.first(), req.second()), ActorRef.noSender());
-                    return HttpResponse
+                    return HttpResponse.create()
                 });
     }
 }
