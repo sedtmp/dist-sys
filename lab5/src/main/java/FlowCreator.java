71fca829404ffd0986c8d393beb3ca426566af49
sedtmp <smegdm@yandex.ru>
2021-12-18 04:44:47 +0300

    Scripted auto-commit on change (2021-12-18 04:44:47) by gitwatch.sh

--- lab5/src/main/java/FlowCreator.java
+++ lab5/src/main/java/FlowCreator.java
@@ -74,7 +74,7 @@ public class FlowCreator {
                                 .via(flow)
                                 .toMat(Sink.fold((int) 0, Integer::sum), Keep.right())
                                 .run(materializer)
-                                .
+                                .thenApply()
                     });
                 })
                 .map(req -> {})
