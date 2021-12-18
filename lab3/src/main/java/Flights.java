45ee19b3ed3813e4ed57461c38f5910556aca3e2
sedtmp <smegdm@yandex.ru>
2021-12-18 12:20:48 +0300

    Scripted auto-commit on change (2021-12-18 12:20:48) by gitwatch.sh

--- lab3/src/main/java/Flights.java
+++ lab3/src/main/java/Flights.java
@@ -33,7 +33,7 @@ public class Flights implements Serializable {
         return flightsCount;
     }
 
-    public static Flights merge(Flights flights, float maxArrDelay, ) {
+    public static Flights merge(Flights flights, float maxArrDelay, boolean isDelayed) {
         if (flightData.getArrDelay() > delayMaxTime) {
             delayMaxTime = flightData.getArrDelay();
         }
