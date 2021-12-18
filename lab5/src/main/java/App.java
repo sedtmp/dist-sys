8b6f4a594b3bcc0b1491ab652edd72b31fc865bc
sedtmp <smegdm@yandex.ru>
2021-12-18 04:50:12 +0300

    Scripted auto-commit on change (2021-12-18 04:50:12) by gitwatch.sh

--- lab5/src/main/java/App.java
+++ lab5/src/main/java/App.java
@@ -7,6 +7,6 @@ public class App {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("lab5");
         ActorRef cacheActor = system.actorOf(Props.create(CacheActor.class));
-        final 
+        final Http
     }
 }
