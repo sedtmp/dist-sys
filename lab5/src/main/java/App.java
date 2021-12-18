b0e0c489ba5025a3911e7908170298ae96e54868
sedtmp <smegdm@yandex.ru>
2021-12-18 04:50:05 +0300

    Scripted auto-commit on change (2021-12-18 04:50:05) by gitwatch.sh

--- lab5/src/main/java/App.java
+++ lab5/src/main/java/App.java
@@ -6,6 +6,7 @@ import akka.actor.Props;
 public class App {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("lab5");
-        ActorRef cacheActor = system.actorOf(Props.create(CacheActor))
+        ActorRef cacheActor = system.actorOf(Props.create(CacheActor.class));
+        
     }
 }
