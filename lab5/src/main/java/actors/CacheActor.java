f14a1ff1b9f2cfa847146a86e6705190f620631a
sedtmp <smegdm@yandex.ru>
2021-12-18 03:34:01 +0300

    Scripted auto-commit on change (2021-12-18 03:34:01) by gitwatch.sh

--- lab5/src/main/java/actors/CacheActor.java
+++ lab5/src/main/java/actors/CacheActor.java
@@ -10,6 +10,6 @@ public class CacheActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
-        return ReceiveBuilder.create().
+        return ReceiveBuilder.create().match()
     }
 }
