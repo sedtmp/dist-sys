86553c4d2df983280e7e528e1f81fefd73d51659
sedtmp <smegdm@yandex.ru>
2021-12-18 04:24:08 +0300

    Scripted auto-commit on change (2021-12-18 04:24:08) by gitwatch.sh

--- lab5/src/main/java/FlowCreator.java
+++ lab5/src/main/java/FlowCreator.java
@@ -45,7 +45,7 @@ public class FlowCreator {
                 })
                 .mapAsync(MAP_ASYNC, req -> {
                     CompletionStage<Object> stage = Patterns.ask(cacheActor, new GetMessage(req.first()), Duration.ofSeconds(TIME_OUT);
-                    return stage.thenCompose()
+                    return stage.thenCompose(res -> )
                 })
                 .map(req -> {})
     }
