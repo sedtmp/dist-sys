a37f02d6acd690bf0308aefb1accfe5bc775ad16
sedtmp <smegdm@yandex.ru>
2021-12-18 04:38:57 +0300

    Scripted auto-commit on change (2021-12-18 04:38:57) by gitwatch.sh

--- lab5/src/main/java/FlowCreator.java
+++ lab5/src/main/java/FlowCreator.java
@@ -54,7 +54,7 @@ public class FlowCreator {
                         if ((Integer) res >= 0) {
                             return CompletableFuture.completedFuture(new Pair<>(req.first(), (Integer) res));
                         }
-                        Flow<Pair<String, Integer>, Integer, NotUsed> flow = 
+                        Flow<Pair<String, Integer>, Integer, NotUsed> flow = Flow.
                     });
                 })
                 .map(req -> {})
