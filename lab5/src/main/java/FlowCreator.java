829175b63bb7addcb5abe8dd1525f87e21a6f52f
sedtmp <smegdm@yandex.ru>
2021-12-18 04:12:54 +0300

    Scripted auto-commit on change (2021-12-18 04:12:54) by gitwatch.sh

--- lab5/src/main/java/FlowCreator.java
+++ lab5/src/main/java/FlowCreator.java
@@ -36,7 +36,7 @@ public class FlowCreator {
                     Query query = req.getUri().query();
                     String url = query.get(TEST_URL).get();
                     int count = Integer.parseInt(query.get(COUNT).get());
-                    System.out.println(url + Constants.SPACE);
+                    System.out.println(url + Constants.SPACE + );
                 })
                 .mapAsync(MAP_ASYNC, req -> {})
                 .map(req -> {})
