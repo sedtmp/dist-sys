17ed6c1b623e2374bfd75ed44dc0f0a5fc903247
sedtmp <smegdm@yandex.ru>
2021-12-18 03:28:21 +0300

    Scripted auto-commit on change (2021-12-18 03:28:21) by gitwatch.sh

--- lab5/src/main/java/messages/GetMessage.java
+++ lab5/src/main/java/messages/GetMessage.java
@@ -3,5 +3,11 @@ package messages;
 public class GetMessage {
     private String url;
 
-    public GetMessage
+    public GetMessage(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
