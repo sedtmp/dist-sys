22660a8d1c0d2429c3b68bd55305ed8d87b73cb6
sedtmp <smegdm@yandex.ru>
2021-12-18 03:53:59 +0300

    Scripted auto-commit on change (2021-12-18 03:53:59) by gitwatch.sh

--- lab5/src/main/java/FlowCreator.java
+++ lab5/src/main/java/FlowCreator.java
@@ -9,7 +9,12 @@ public class FlowCreator {
     private final ActorRef cacheActor;
     private final Materializer materializer;
 
-    public FlowCreator(ActorRef cacheActor, Materializer materializer) {
+    public FlowCreator(Http http,
+                       ActorSystem actorSystem,
+                       ActorRef cacheActor,
+                       Materializer materializer) {
+        this.http = http;
+        this.system = 
         this.cacheActor = cacheActor;
         this.materializer = materializer;
     }
