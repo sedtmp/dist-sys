2749c6d6937b97e80549f743a3a568f355ded1f1
sedtmp <smegdm@yandex.ru>
2021-12-25 02:34:49 +0300

    Scripted auto-commit on change (2021-12-25 02:34:49) by gitwatch.sh

--- lab6/src/main/java/ZooWatcher.java
+++ lab6/src/main/java/ZooWatcher.java
@@ -6,5 +6,5 @@ public class ZooWatcher implements Watcher {
     private ZooKeeper zoo;
     private ActorRef storage;
 
-    public ZooWatcher(ZooKeeper zoo, ActorRef storage)
+    public ZooWatcher(ZooKeeper zoo, ActorRef storage) throws 
 }
