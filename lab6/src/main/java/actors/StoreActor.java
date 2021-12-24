12579009caf033393a4414e24879dd40c50e6cf2
sedtmp <smegdm@yandex.ru>
2021-12-25 01:38:13 +0300

    Scripted auto-commit on change (2021-12-25 01:38:13) by gitwatch.sh

--- lab6/src/main/java/actors/StoreActor.java
+++ lab6/src/main/java/actors/StoreActor.java
@@ -20,6 +20,6 @@ public class StoreActor extends AbstractActor {
     }
 
     private String getRandomServer() {
-        return servers.get(random)
+        return servers.get(random.nextInt())
     }
 }
