8dda14508f824b953c989efc6d9d6f02980f268a
sedtmp <smegdm@yandex.ru>
2021-12-25 01:37:07 +0300

    Scripted auto-commit on change (2021-12-25 01:37:07) by gitwatch.sh

--- lab6/src/main/java/actors/StoreActor.java
+++ lab6/src/main/java/actors/StoreActor.java
@@ -15,5 +15,6 @@ public class StoreActor extends AbstractActor {
     public Receive createReceive() {
         return receiveBuilder()
                 .match(StoreMessage.class, msg -> this.servers = msg.getServers())
+                .match()
     }
 }
