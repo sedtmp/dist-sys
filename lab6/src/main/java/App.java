83e432b25e4c63008380e40475a586f9b6f31669
sedtmp <smegdm@yandex.ru>
2021-12-25 04:39:02 +0300

    Scripted auto-commit on change (2021-12-25 04:39:02) by gitwatch.sh

--- lab6/src/main/java/App.java
+++ lab6/src/main/java/App.java
@@ -4,6 +4,7 @@ import org.apache.log4j.BasicConfigurator;
 public class App {
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        ActorSystem actorSystem = ActorSystem.create()
+        ActorSystem actorSystem = ActorSystem.create("routes");
+        
     }
 }
