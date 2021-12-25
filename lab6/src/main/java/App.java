fc603e6dbdd627deb86ae543054e346c7c6e88be
sedtmp <smegdm@yandex.ru>
2021-12-25 04:38:44 +0300

    Scripted auto-commit on change (2021-12-25 04:38:44) by gitwatch.sh

--- lab6/src/main/java/App.java
+++ lab6/src/main/java/App.java
@@ -1,8 +1,9 @@
+import akka.actor.ActorSystem;
 import org.apache.log4j.BasicConfigurator;
 
 public class App {
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        
+        ActorSystem
     }
 }
