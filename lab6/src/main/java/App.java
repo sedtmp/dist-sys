a036ab13c52b0beca8e677333998b2cabc261018
sedtmp <smegdm@yandex.ru>
2021-12-25 04:39:33 +0300

    Scripted auto-commit on change (2021-12-25 04:39:33) by gitwatch.sh

--- lab6/src/main/java/App.java
+++ lab6/src/main/java/App.java
@@ -1,11 +1,12 @@
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import org.apache.log4j.BasicConfigurator;
 
 public class App {
     public static void main(String[] args) {
         BasicConfigurator.configure();
         ActorSystem actorSystem = ActorSystem.create("routes");
-        ActorRef storage = actorSystem.actorOf()
+        ActorRef storage = actorSystem.actorOf(Props.create())
     }
 }
