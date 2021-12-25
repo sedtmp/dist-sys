07e05521d618ca75af13d83ffc2f5ecde34a3c66
sedtmp <smegdm@yandex.ru>
2021-12-25 05:29:25 +0300

    Scripted auto-commit on change (2021-12-25 05:29:25) by gitwatch.sh

--- lab6/src/main/java/App.java
+++ lab6/src/main/java/App.java
@@ -69,7 +69,7 @@ public class App {
         }
 
         for (CompletionStage<ServerBinding> binding : bindings) {
-            binding.thenCompose(ServerBinding::unbind).thenAccept()
+            binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> )
         }
     }
 }
