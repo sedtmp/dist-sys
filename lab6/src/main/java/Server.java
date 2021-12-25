1bf0d75381a2ff882099ae8cd0cbabbb306582fd
sedtmp <smegdm@yandex.ru>
2021-12-25 04:01:28 +0300

    Scripted auto-commit on change (2021-12-25 04:01:28) by gitwatch.sh

--- lab6/src/main/java/Server.java
+++ lab6/src/main/java/Server.java
@@ -36,7 +36,7 @@ public class Server implements Watcher {
     }
 
     private static Route check(ActorRef actorConfig, Http http, Request request) {
-        if (request)
+        if ()
     }
 
     public Route createRoute() {
