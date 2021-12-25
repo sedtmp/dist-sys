531b9efaf6fb7072cb93f7b431c8bf3379b6f5b2
sedtmp <smegdm@yandex.ru>
2021-12-25 03:15:13 +0300

    Scripted auto-commit on change (2021-12-25 03:15:13) by gitwatch.sh

--- lab6/src/main/java/Server.java
+++ lab6/src/main/java/Server.java
@@ -1,6 +1,7 @@
+import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
 import org.apache.zookeeper.Watcher;
 
 public class Server implements Watcher {
-    public class Server(Http, )
+    public class Server(Http, ActorRef )
 }
