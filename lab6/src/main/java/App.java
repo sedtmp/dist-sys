3c914b9454736631bdea3854e369f9b4f27877bb
sedtmp <smegdm@yandex.ru>
2021-12-25 05:28:59 +0300

    Scripted auto-commit on change (2021-12-25 05:28:59) by gitwatch.sh

--- lab6/src/main/java/App.java
+++ lab6/src/main/java/App.java
@@ -68,6 +68,8 @@ public class App {
             System.exit(-1);
         }
 
-        for (CompletionStage<ServerBinding> )
+        for (CompletionStage<ServerBinding> binding : bindings) {
+            
+        }
     }
 }
