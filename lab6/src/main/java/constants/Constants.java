d97e1613b85fa570667fd7b798367013c8d36e9c
sedtmp <smegdm@yandex.ru>
2021-12-25 05:04:06 +0300

    Scripted auto-commit on change (2021-12-25 05:04:06) by gitwatch.sh

--- lab6/src/main/java/constants/Constants.java
+++ lab6/src/main/java/constants/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String SERVER_PATH_PATTERN = SERVERS_PATH + "/" + "%s";
     public static final String HOST = "localhost";
     public static final String EMPTY_STRING = "";
-    public static final String
+    public static final int ZOOKEEPER_ADDRESS
 
     public static String createServerPath(String port) {
         return String.format("%s:%s", HOST, port);
