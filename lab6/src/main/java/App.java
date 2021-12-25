9a972871e469c59348fec3b4bc0d3d005e9b5ead
sedtmp <smegdm@yandex.ru>
2021-12-25 05:06:31 +0300

    Scripted auto-commit on change (2021-12-25 05:06:31) by gitwatch.sh

--- lab6/src/main/java/App.java
+++ lab6/src/main/java/App.java
@@ -12,7 +12,7 @@ import org.apache.zookeeper.ZooKeeper;
 import java.io.IOException;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  {
         BasicConfigurator.configure();
         ActorSystem actorSystem = ActorSystem.create("routes");
         ActorRef storage = actorSystem.actorOf(Props.create(StoreActor.class));
