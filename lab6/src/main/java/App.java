f73994c0c2957aa90e6f94fdbeea97590b0d7b18
sedtmp <smegdm@yandex.ru>
2021-12-25 04:43:04 +0300

    Scripted auto-commit on change (2021-12-25 04:43:04) by gitwatch.sh

--- lab6/src/main/java/App.java
+++ lab6/src/main/java/App.java
@@ -12,6 +12,6 @@ public class App {
         ActorSystem actorSystem = ActorSystem.create("routes");
         ActorRef storage = actorSystem.actorOf(Props.create(StoreActor.class));
         final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
-        final Http http = Http
+        final Http http = Http.get()
     }
 }
