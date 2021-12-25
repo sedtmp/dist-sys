09611a856b611cb63c478766a8c7fc49331db3e1
sedtmp <smegdm@yandex.ru>
2021-12-25 03:15:44 +0300

    Scripted auto-commit on change (2021-12-25 03:15:44) by gitwatch.sh

--- lab6/src/main/java/Server.java
+++ lab6/src/main/java/Server.java
@@ -4,5 +4,7 @@ import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
 public class Server implements Watcher {
-    public class Server(Http, ActorRef actorConfig, ZooKeeper zoo, String port)
+    public class Server(Http ActorRef actorConfig, ZooKeeper zoo, String port) {
+
+    }
 }
