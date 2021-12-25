26b31e41584b90af633abd5b51c10d2a458dfad3
sedtmp <smegdm@yandex.ru>
2021-12-25 04:42:57 +0300

    Scripted auto-commit on change (2021-12-25 04:42:57) by gitwatch.sh

--- lab6/src/main/java/App.java
+++ lab6/src/main/java/App.java
@@ -2,6 +2,7 @@ import actors.StoreActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
 import org.apache.log4j.BasicConfigurator;
 
@@ -11,6 +12,6 @@ public class App {
         ActorSystem actorSystem = ActorSystem.create("routes");
         ActorRef storage = actorSystem.actorOf(Props.create(StoreActor.class));
         final ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
-        final Http http 
+        final Http http =
     }
 }
