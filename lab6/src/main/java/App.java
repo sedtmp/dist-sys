f77e0153f4985d37b38dc27b8e26e16e70f1f88a
sedtmp <smegdm@yandex.ru>
2021-12-25 05:19:05 +0300

    Scripted auto-commit on change (2021-12-25 05:19:05) by gitwatch.sh

--- lab6/src/main/java/App.java
+++ lab6/src/main/java/App.java
@@ -41,7 +41,7 @@ public class App {
         for (int i = 1; i < args.length; i++) {
             try {
                 Server server = new Server(http, storage, zoo, args[i]);
-                final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = 
+                final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = server.createRoute()
             }
         }
 
