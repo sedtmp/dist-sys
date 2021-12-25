de1681ba9be967daac7771f000922bb0d5d5c6d3
sedtmp <smegdm@yandex.ru>
2021-12-25 03:02:14 +0300

    Scripted auto-commit on change (2021-12-25 03:02:14) by gitwatch.sh

--- lab6/src/main/java/ZooWatcher.java
+++ lab6/src/main/java/ZooWatcher.java
@@ -20,7 +20,7 @@ public class ZooWatcher implements Watcher {
         List<String> servers = new ArrayList<>();
         for (String server : zoo.getChildren(Constants.SERVERS_PATH, this)) {
             servers.add(new String(zoo.getData(String.format(Constants.SERVER_PATH_PATTERN, server), false, null)));
-            
         }
+        storage.tell();
     }
 }
