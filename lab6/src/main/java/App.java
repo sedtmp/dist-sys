66f76e2f90d3338bae92f8058fe3c5f032a6cabd
sedtmp <smegdm@yandex.ru>
2021-12-25 04:57:08 +0300

    Scripted auto-commit on change (2021-12-25 04:57:08) by gitwatch.sh

--- lab6/src/main/java/App.java
+++ lab6/src/main/java/App.java
@@ -10,7 +10,7 @@ import org.apache.zookeeper.ZooKeeper;
 public class App {
     public static void main(String[] args) {
         if (args.length < 2) {
-            System.out.println();
+            System.out.println("Usage");
         }
         BasicConfigurator.configure();
         ActorSystem actorSystem = ActorSystem.create("routes");
